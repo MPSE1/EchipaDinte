74af80020f70b530e4c3d3d6005e721b75c87aa4
MihaiNiculae <MihaiNiculae@IBM-MNiculae>
2016-11-10 20:33:51 +0200

    Added map file. Client now reads map. Collisions WIP

--- client/src/client/Main.java
+++ client/src/client/Main.java
@@ -1,12 +1,16 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Vector;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -14,42 +18,154 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
 	public static Rectangle r;
+	public static final int speed = 5;
+	public static final int playerSize = 25;
 	public static Vector<Rectangle> players = new Vector<Rectangle>();
 	public static Pane root;
+	public static int [][] map = new int[800][800];
 	
 	@Override
 	public void start(Stage primaryStage) {
         primaryStage.setTitle("Game");
-        r.setX(50);
-        r.setY(50);
-        r.setWidth(25);
-        r.setHeight(25);
+        r.setX(0);
+        r.setY(0);
+        r.setWidth(playerSize);
+        r.setHeight(playerSize);
         r.setArcWidth(20);
         r.setArcHeight(20);
         r.setFill(Color.AQUA);
 
         
         root = new Pane();
+        Rectangle rectangle = new Rectangle(600,600,Color.LIGHTGREY);
+		root.getChildren().add(rectangle);
+		readMap("map1.txt");
         root.getChildren().add(r);
-        Scene scene = new Scene(root, 800, 800);
+        
+        Scene scene = new Scene(root, 1280, 720);
 		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
 
 			@Override
 			public void handle(KeyEvent event) {
 				switch (event.getCode()) {
-                   case UP:    Connect.state.posY -= 3; break;
-                   case DOWN:  Connect.state.posY += 3; break;
-                   case LEFT:  Connect.state.posX -= 3; break;
-                   case RIGHT: Connect.state.posX += 3; break;
+                   case UP:    
+                	   if (checkCollisions(KeyCode.UP))
+                		   Connect.state.posY -= speed; 
+                	   break;
+                   case DOWN:  
+                	   if (checkCollisions(KeyCode.DOWN))
+                		   Connect.state.posY += speed; 
+                	   break;
+                   case LEFT:  
+                	   if (checkCollisions(KeyCode.LEFT))
+                		   Connect.state.posX -= speed;
+                	   break;
+                   case RIGHT: 
+                	   if (checkCollisions(KeyCode.RIGHT))
+                		   Connect.state.posX += speed;
+                	   break;
 				default:
 					break;
 				}
+			}
+
+			private boolean checkCollisions(KeyCode up) {
+//				switch (up) {
+//				case UP:
+//					int x = Connect.state.posX - speed;
+//					int y = Connect.state.posY;
+//					if( x< 0 || x >= 800 || y<0 || y>= 800)
+//						return false;
+//					if (map[x][y] == 1)
+//						return false;
+//					
+//					if (map[x + playerSize][y] == 1)
+//						return false;
+//					break;
+//				case DOWN:
+//					 x = Connect.state.posX + speed;
+//					 y = Connect.state.posY;
+//					 if( x< 0 || x >= 800 || y<0 || y>= 800)
+//							return false;
+//					if (map[x+ playerSize][y+ playerSize] == 1)
+//						return false;
+//					
+//					if (map[x + playerSize][y] == 1)
+//						return false;
+//					break;
+//					
+//				case LEFT:
+//					 x = Connect.state.posX ;
+//					 y = Connect.state.posY - speed;
+//					 if( x< 0 || x >= 800 || y<0 || y>= 800)
+//							return false;
+//					if (map[x][y] == 1)
+//						return false;
+//					
+//					if (map[x + playerSize][y] == 1)
+//						return false;
+//					break;
+//				case RIGHT:
+//					 x = Connect.state.posX ;
+//					 y = Connect.state.posY + speed;
+//					 if( x< 0 || x >= 800 || y<0 || y>= 800)
+//							return false;
+//					if (map[x][y+ playerSize] == 1)
+//						return false;
+//					
+//					if (map[x + playerSize][y + playerSize] == 1)
+//						return false;
+//					break;	
+//				default:
+//					break;
+//				}
+				return true;
 			}
 		});
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 	
+	public void readMap(String fileName){
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			int nr = Integer.parseInt(br.readLine());
+			
+			for (int i = 0; i < nr; i++) {
+				String[] line = br.readLine().split(" ");
+				int x,y,width,height;
+				x = Integer.parseInt(line[0]);
+				y = Integer.parseInt(line[1]);
+				width = Integer.parseInt(line[2]);
+				height = Integer.parseInt(line[3]);
+				Rectangle r = new Rectangle(width, height, Color.MAGENTA);
+				r.setX(x);
+				r.setY(y);
+				root.getChildren().add(r);
+				addToMap(x,y,width,height);
+
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void addToMap(int x, int y, int width, int height) {
+		for (int i = x; i < x+ width; i++) {
+			for (int j = y; j < y+height; j++) {
+				map[i][j] = 1 ;
+			}
+		}
+		
+	}
+
 	public static void main(String [] args) throws IOException{
         r = new Rectangle();
 		Connect connection = new Connect();
